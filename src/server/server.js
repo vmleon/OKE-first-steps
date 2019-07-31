5668a009169e59e92f955da5e0e8f161cbef4e85
Victor Martin <victor.martin.alvarez@oracle.com>
2019-07-31 15:44:44 +0100

    Server, /vote and /health endpoints

---
+++ src/server/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const port = process.env.PORT || 3000
+
+let poll = { "yes": 0, "no": 0 }
+
+const responseMapper = vote => vote ? "yes" : "no";
+
+const createResponse = (data, message) => {
+    if (message) {
+        return {
+            data,
+            error: true,
+            message
+        }
+    }
+    return ({ data })
+}
+
+app.get('/vote', (req, res) => res.json(createResponse(poll)))
+
+app.post('/vote', (req, res) => {
+    const result = { vote: responseMapper(req.body.vote), created: Date.now().toString() }
+    result.vote === "yes" ? poll.yes++ : poll.no++
+    return res.status(200).json(createResponse(result))
+})
+
+app.delete('/vote', (req, res) => {
+    poll = { "yes": 0, "no": 0 }
+    return res.status(200).json(createResponse(poll))
+})
+
+app.get('/health', (req, res) => res.json({ status: 'UP' }))
+
+app.use((req, res) => res.send(404));
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
