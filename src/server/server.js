2344fee7d8034df7ec05ac254626d0658a1013c2
Victor Martin <victor.martin.alvarez@oracle.com>
2020-01-10 10:49:19 +0000

    Improve ingress controller, but still WIP

--- src/server/server.js
+++ src/server/server.js
@@ -1,38 +1,44 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
 
-let poll = { "yes": 0, "no": 0 }
+let poll = { yes: 0, no: 0 };
 
-const responseMapper = vote => vote ? "yes" : "no";
+const responseMapper = vote => (vote ? "yes" : "no");
 
 const createResponse = (data, message) => {
-    if (message) {
-        return {
-            data,
-            error: true,
-            message
-        }
-    }
-    return ({ data })
-}
+  if (message) {
+    return {
+      data,
+      error: true,
+      message
+    };
+  }
+  return { data };
+};
 
-app.get('/vote', (req, res) => res.json(createResponse(poll)))
+app.get("/vote", (req, res) => res.json(createResponse(poll)));
 
-app.post('/vote', (req, res) => {
-    const result = { vote: responseMapper(req.body.vote), created: Date.now().toString() }
-    result.vote === "yes" ? poll.yes++ : poll.no++
-    return res.status(200).json(createResponse(result))
-})
+app.post("/vote", (req, res) => {
+  const result = {
+    vote: responseMapper(req.body.vote),
+    created: Date.now().toString()
+  };
+  result.vote === "yes" ? poll.yes++ : poll.no++;
+  return res.json(createResponse(result));
+});
 
-app.delete('/vote', (req, res) => {
-    poll = { "yes": 0, "no": 0 }
-    return res.status(200).json(createResponse(poll))
-})
+app.delete("/vote", (req, res) => {
+  poll = { yes: 0, no: 0 };
+  return res.json(createResponse(poll));
+});
 
-app.get('/health', (req, res) => res.json({ status: 'UP' }))
+app.get("/health", (req, res) => res.json({ status: "UP" }));
 
-app.use((req, res) => res.send(404));
+app.use((req, res) => {
+  console.log({ baseURL: req.baseUrl, path: req.path });
+  return res.sendStatus(404);
+});
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
